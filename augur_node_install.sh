bbd3438d9bcdbced2803674ec5bbe961d12f9a49
Kevin Day <kevin.r.day@gmail.com>
2016-07-06 14:39:58 -0500

    adding fast sync, node operator responsible for managing own ports.

--- augur_node_install.sh
+++ augur_node_install.sh
@@ -69,21 +69,6 @@ time sudo apt-get -y install git build-essential
 sudo -i -u $AUGURUSER git clone https://github.com/AugurProject/marketeer.git
 sudo -i -u $AUGURUSER  bash -c "cd marketeer; npm install"
 
-####################
-#Open ports?
-####################
-#declare -a ports=("8545" "8546" "8547" "30303" "30304")
-#for port in "${ports[@]}"; do
-#    UDP="INPUT -p udp --dport ${port} -j ACCEPT"
-#    TCP="INPUT -p tcp --dport ${port} -j ACCEPT"
-#    set +e
-#    sudo iptables -D $UDP >> /dev/null 2>&1
-#   sudo iptables -D $TCP >> /dev/null 2>&1
-#    set -e
-#    sudo iptables -A $UDP
-#    sudo iptables -A $TCP
-#    echo -e "Opened port ${port}"
-#done
 
 ####################
 #Install and Start augur_node service
