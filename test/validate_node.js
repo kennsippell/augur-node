d3d2029866128906637573d28945c657fecd58bd
Kevin Day <kevin.r.day@gmail.com>
2016-08-25 20:04:44 -0500

    Adding priceHistory test

--- test/validate_node.js
+++ test/validate_node.js
@@ -59,42 +59,69 @@ describe("marketInfo", function () {
 		this.timeout(TIMEOUT);
 		var url = node + "/getMarketsInfo";
 		request(url, function (error, response, body) {
-		  	if (!error && response.statusCode == 200) {
-		  		var cacheMarkets = JSON.parse(body);
+			assert.isNull(error);
+			assert.deepEqual(response.statusCode, 200);
+	  		var cacheMarkets = JSON.parse(body);
 
-				var marketsPerPage = 50;
-				var numMarkets = parseInt(augur.getNumMarketsBranch(augur.constants.DEFAULT_BRANCH_ID));
-				var numPages = Math.ceil(numMarkets / Number(marketsPerPage));
+			var marketsPerPage = 50;
+			var numMarkets = parseInt(augur.getNumMarketsBranch(augur.constants.DEFAULT_BRANCH_ID));
+			var numPages = Math.ceil(numMarkets / Number(marketsPerPage));
 
-				var range = new Array(numPages);
-				for (var i = 0; i < numPages; ++i) {
-				     range[numPages - i - 1] = i*marketsPerPage;
-				}
-				var markets = {};
-				console.log("numMarkets", numMarkets);
-				async.forEachOfSeries(range, (offset, index, next) => {
-					var numMarketsToLoad = (index === 0) ? numMarkets - range[index] : marketsPerPage;
-					console.log("offset:", offset);
-					augur.getMarketsInfo({
-						branch: augur.constants.DEFAULT_BRANCH_ID,
-						offset: offset,
-						numMarketsToLoad: numMarketsToLoad,
-						callback: function (marketsInfo) {
-							if (!marketsInfo || marketsInfo.error) return next(marketsInfo || "getMarketsInfo");
-							for(var key in marketsInfo){
-				  				if(marketsInfo.hasOwnProperty(key)){
-				    				markets[key]=marketsInfo[key];
-				  				}
-							}
-							next();
-						}
-					});
-				}, (err) => {
-				  		assert.isTrue(sameIds(markets, cacheMarkets));
-				  		assert.isTrue(marketsInfoEqual(markets, cacheMarkets));
-						done();
-				});
+			var range = new Array(numPages);
+			for (var i = 0; i < numPages; ++i) {
+			     range[numPages - i - 1] = i*marketsPerPage;
 			}
+			var markets = {};
+			console.log("numMarkets", numMarkets);
+			async.forEachOfSeries(range, (offset, index, next) => {
+				var numMarketsToLoad = (index === 0) ? numMarkets - range[index] : marketsPerPage;
+				console.log("offset:", offset);
+				augur.getMarketsInfo({
+					branch: augur.constants.DEFAULT_BRANCH_ID,
+					offset: offset,
+					numMarketsToLoad: numMarketsToLoad,
+					callback: function (marketsInfo) {
+						if (!marketsInfo || marketsInfo.error) return next(marketsInfo || "getMarketsInfo");
+						for(var key in marketsInfo){
+			  				if(marketsInfo.hasOwnProperty(key)){
+			    				markets[key]=marketsInfo[key];
+			  				}
+						}
+						next();
+					}
+				});
+			}, (err) => {
+			  		assert.isTrue(sameIds(markets, cacheMarkets));
+			  		assert.isTrue(marketsInfoEqual(markets, cacheMarkets));
+					done();
+			});
+		});
+	});
+});
+
+
+describe("priceHistory", function () {
+	it("matches data on chain", function (done) {
+		this.timeout(TIMEOUT);
+
+		var markets = augur.getMarketsInBranch(augur.constants.DEFAULT_BRANCH_ID);
+		async.each(markets, function (id, nextMarket){
+			var chainHistory = augur.getMarketPriceHistory(id);
+			if (chainHistory == null){
+				nextMarket();
+				return;
+			}
+			var url = node + "/getMarketPriceHistory?id=" + id;
+			request(url, function (error, response, body) {
+				assert.isNull(error);
+				assert.deepEqual(response.statusCode, 200);
+		  		var cacheHistory = JSON.parse(body);
+		  		assert.deepEqual(JSON.stringify(chainHistory), JSON.stringify(cacheHistory));
+		  		nextMarket();
+		  	});		
+		}, (err) => {
+			assert.isNull(err);
+			done();
 		});
 	});
 });
