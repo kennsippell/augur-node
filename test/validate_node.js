ccd42e10408169f55ae36dc378aff21b08b43a31
Kevin Day <kevin.r.day@gmail.com>
2016-08-25 19:18:41 -0500

    turning this into a proper mocha test

--- test/validate_node.js
+++ test/validate_node.js
@@ -2,6 +2,7 @@
 var request = require('request');
 var augur = require("augur.js");
 var async = require("async");
+var assert = require("chai").assert;
 
 var config = {
     http: "http://localhost:8545",
@@ -12,24 +13,28 @@ augur.connect(config);
 
 var args = process.argv.slice(2);
 var port = "8547";
-var url = "http://" + args[0] + ":" + port + "/getMarketsInfo";
-
-console.log(url);
+var node = "http://augurnode.eastus.cloudapp.azure.com:8547";
+var TIMEOUT = 60000;
 
 function sameIds(chain, cache){
+	var same = true;
 	for (var key in chain){
 		if (!cache.hasOwnProperty(key)){
+			same = false;
 			console.log(key, "on chain, not in cache");
 		}
 	}
 	for (var key in chain){
 		if (!cache.hasOwnProperty(key)){
+			same = false;
 			console.log(key, "in cache, not on chain");
 		}
 	}
+	return same;
 }
 
 function marketsInfoEqual(chain, cache){
+	var same = true;
 	for (var key in chain){
 		if(chain.hasOwnProperty(key)){
 			var chainMarket = chain[key];
@@ -37,6 +42,7 @@ function marketsInfoEqual(chain, cache){
 
 			for (var prop in cacheMarket){
 				if(cacheMarket.hasOwnProperty(prop)){
+					same = false;
 					if (chainMarket[prop].constructor == Array) continue;
 					if (cacheMarket[prop] != chainMarket[prop]){
 						console.log(key, prop, "chain:", chainMarket[prop], "cache:", cacheMarket[prop]);
@@ -45,46 +51,53 @@ function marketsInfoEqual(chain, cache){
 			}
 		}
 	}
+	return same;
 }
 
-request(url, function (error, response, body) {
-  	if (!error && response.statusCode == 200) {
-  		var cacheMarkets = JSON.parse(body);
+describe("marketInfo", function () {
+	it("matches data on chain", function (done) {
+		this.timeout(TIMEOUT);
+		var url = node + "/getMarketsInfo";
+		request(url, function (error, response, body) {
+		  	if (!error && response.statusCode == 200) {
+		  		var cacheMarkets = JSON.parse(body);
 
-		var marketsPerPage = 50;
-		var numMarkets = parseInt(augur.getNumMarketsBranch(augur.constants.DEFAULT_BRANCH_ID));
-		var numPages = Math.ceil(numMarkets / Number(marketsPerPage));
+				var marketsPerPage = 50;
+				var numMarkets = parseInt(augur.getNumMarketsBranch(augur.constants.DEFAULT_BRANCH_ID));
+				var numPages = Math.ceil(numMarkets / Number(marketsPerPage));
 
-		var range = new Array(numPages);
-		for (var i = 0; i < numPages; ++i) {
-		     range[numPages - i - 1] = i*marketsPerPage;
-		}
-		var markets = {};
-		console.log("numMarkets", numMarkets);
-		async.forEachOfSeries(range, (offset, index, next) => {
-			var numMarketsToLoad = (index === 0) ? numMarkets - range[index] : marketsPerPage;
-			console.log("offset:", offset);
-			augur.getMarketsInfo({
-				branch: augur.constants.DEFAULT_BRANCH_ID,
-				offset: offset,
-				numMarketsToLoad: numMarketsToLoad,
-				callback: function (marketsInfo) {
-					if (!marketsInfo || marketsInfo.error) return next(marketsInfo || "getMarketsInfo");
-					for(var key in marketsInfo){
-		  				if(marketsInfo.hasOwnProperty(key)){
-		    				markets[key]=marketsInfo[key];
-		  				}
-					}
-					next();
+				var range = new Array(numPages);
+				for (var i = 0; i < numPages; ++i) {
+				     range[numPages - i - 1] = i*marketsPerPage;
 				}
-			});
-		}, (err) => {
-		  		sameIds(markets, cacheMarkets);
-		  		marketsInfoEqual(markets, cacheMarkets);
+				var markets = {};
+				console.log("numMarkets", numMarkets);
+				async.forEachOfSeries(range, (offset, index, next) => {
+					var numMarketsToLoad = (index === 0) ? numMarkets - range[index] : marketsPerPage;
+					console.log("offset:", offset);
+					augur.getMarketsInfo({
+						branch: augur.constants.DEFAULT_BRANCH_ID,
+						offset: offset,
+						numMarketsToLoad: numMarketsToLoad,
+						callback: function (marketsInfo) {
+							if (!marketsInfo || marketsInfo.error) return next(marketsInfo || "getMarketsInfo");
+							for(var key in marketsInfo){
+				  				if(marketsInfo.hasOwnProperty(key)){
+				    				markets[key]=marketsInfo[key];
+				  				}
+							}
+							next();
+						}
+					});
+				}, (err) => {
+				  		assert.isTrue(sameIds(markets, cacheMarkets));
+				  		assert.isTrue(marketsInfoEqual(markets, cacheMarkets));
+						done();
+				});
+			}
 		});
-	}
+	});
 });
-
 
 
 
