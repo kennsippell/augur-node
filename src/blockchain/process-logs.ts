d224a696c217cc5d3f6a61392f420c15dee3cc51
Scott Bigelow <epheph@gmail.com>
2017-11-21 10:27:19 -0800

    quick hack to avoid null

--- src/blockchain/process-logs.ts
+++ src/blockchain/process-logs.ts
@@ -4,7 +4,13 @@ import * as Knex from "knex";
 import { FormattedLog, EventLogProcessor, ErrorCallback } from "../types";
 
 export function processLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedLog, logProcessor: EventLogProcessor, callback: ErrorCallback): void {
-  (!log.removed ? logProcessor.add : logProcessor.remove)(db, augur, trx, log, callback);
+
+  if (logProcessor == null ) {
+    console.log("NULL logProcessor");
+    console.log(log);
+  } else {
+    (!log.removed ? logProcessor.add : logProcessor.remove)(db, augur, trx, log, callback);
+  }
 }
 
 export function processLogs(db: Knex, augur: Augur, trx: Knex.Transaction, logs: Array<FormattedLog>, logProcessor: EventLogProcessor, callback: ErrorCallback): void {
