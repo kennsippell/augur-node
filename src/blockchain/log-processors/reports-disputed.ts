d976828f11e38413ee47ede40ca7856ebb46cd23
Scott Bigelow <epheph@gmail.com>
2017-11-30 13:02:55 -0800

    first pass at reports-disputed

--- src/blockchain/log-processors/reports-disputed.ts
+++ src/blockchain/log-processors/reports-disputed.ts
@@ -2,10 +2,29 @@ import Augur from "augur.js";
 import * as Knex from "knex";
 import { FormattedEventLog, ErrorCallback } from "../../types";
 
+// 21:    event ReportsDisputed(address indexed universe, 
+// address indexed disputer, 
+// address indexed market, 
+// IMarket.ReportingState 
+// reportingPhase, uint256 disputeBondAmount);
+
 export function processReportsDisputedLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
-  console.log("TODO: ReportsDisputed");
-  console.log(log);
-  callback(null);
+  const query = trx("market_state").first(["market_state.reportingState as reportingState", "stakeToken"]).join("reports", "market_state.marketID", "reports.marketID")
+  query.where({"markets.marketID": log.market, "reports.marketReportingState": augur.constants.ReportingState.DESIGNATED_REPORTING }).asCallback( (err: Error|null, { reportingState }: {reportingState: string}): void => {
+    if (err) return callback(err);
+    const reportDataToInsert: { [index: string]: string|number|boolean } = {
+      marketID: log.market,
+      stakeToken: log.stakeToken,
+      blockNumber: log.blockNumber,
+      reporter: log.reporter,
+      transactionHash: log.transactionHash,
+      logIndex: log.logIndex,
+      amountStaked: log.amountStaked,
+      marketReportingState: reportingState,
+      claimed: 0,
+    };
+    db.transacting(trx).insert(reportDataToInsert).into("reports").asCallback(callback);
+  });
 }
 
 export function processReportsDisputedLogRemoval(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
