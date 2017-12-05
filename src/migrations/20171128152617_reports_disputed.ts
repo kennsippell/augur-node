01dd3e4714b6c9a1b0b6c23046bb0ab05a5619e3
Scott Bigelow <epheph@gmail.com>
2017-12-04 19:59:32 -0800

    better schema

--- src/migrations/20171128152617_reports_disputed.ts
+++ src/migrations/20171128152617_reports_disputed.ts
@@ -1,19 +1,16 @@
 import * as Knex from "knex";
 
 exports.up = async (knex: Knex): Promise<any> => {
-  return knex.schema.dropTableIfExists("stake_tokens").then( (): PromiseLike<any> => {
-    return knex.schema.createTable("stake_tokens", (table: Knex.CreateTableBuilder): void => {
-      table.string("stakeToken", 42).primary().notNullable();
+  return knex.schema.dropTableIfExists("reports_disputed").then( (): PromiseLike<any> => {
+    return knex.schema.createTable("reports_disputed", (table: Knex.CreateTableBuilder): void => {
+      table.string("reporter", 42).notNullable();      
+      table.string("disputedStakeToken", 42).primary().notNullable();
       table.string("marketID", 42).notNullable();
-      for (let i: number = 0; i <= 7; i++ ) {
-        table.specificType(`payout${i}`, "NUMERIC").nullable();
-      }
-      table.integer("isInvalid");
-      table.index(["marketID"]);
+      table.string("marketReportingState", 30).notNullable();      
     });
   });
 };
 
 exports.down = async (knex: Knex): Promise<any> => {
-  return knex.schema.dropTableIfExists("stake_tokens");
+  return knex.schema.dropTableIfExists("reports_disputed");
 };
